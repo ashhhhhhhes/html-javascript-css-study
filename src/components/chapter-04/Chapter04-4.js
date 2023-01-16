22ea3e2de69e30742539a350c67ccc65a44c3c6c
ash <email@example.com>
2023-01-17 00:08:56 +0900

    예제 이미지 추가

--- src/components/chapter-04/Chapter04-4.js
+++ src/components/chapter-04/Chapter04-4.js
@@ -7,7 +7,8 @@ import CodeEditorWrap from '../common/CodeEditorWrap'
 const Chapter04e04 = () => {
    const [code, setCode] = React.useState('<img src="" alt=""/>');
 
-   const eg = '<img src="" alt=""/>'
+   const egGifImg = '<img src="/images/gif-이미지.gif" alt=""/>'
+   const egJpgmg = '<img src="/images/jpg-이미지.jpg" alt=""/>'
    return (
         <div className="Contents-item">
          <div className="Contents-desc">
@@ -19,10 +20,21 @@ const Chapter04e04 = () => {
            <p>
             기본형: {`<img src="이미지 파일 경로" alt="대체용 텍스트" >`} 으로 사용 가능해요.
 
-            <span className="example" onClick={() => setCode(eg)} >(👇 예시보기)</span> 
+            <span className="example" onClick={() => setCode(egJpgmg)} >(👇 예시보기)</span> 
 
            </p>
 
+            
+           <p>
+            gif 이미지도 가능해요.
+           <span className="example" onClick={() => setCode(egGifImg)} >(👇 예시보기)</span> 
+           </p>
+
+
+           <p>
+            png 이미지도 가능해요.
+           <span className="example" onClick={() => setCode(egPngImg)} >(👇 예시보기)</span> 
+           </p>
          </div>
 
           <CodeEditorWrap data={code} setCode={(text) => { setCode(text)}}/>
